dcda397234b394221c264e12e838244ff5ff35b0
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-07-03 21:30:00 +0200

    added text to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,40 @@ const IndexPage = () => {
         </h2>
         <div className="text-container text-container--2">
           <span>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum,
-            excepturi accusamus cum vel fugiat inventore?
+            W obecnych czasach każdy w bardzo łatwy i tani sposób jest w stanie
+            pokazać się w internecie. Co za tym idzie, nakłanianie do użycia
+            twojego produktu staje się nieefektywne.
+          </span>
+        </div>
+        <div className="line line-4"></div>
+      </section>
+      <section className="section">
+        <div className="line line-5"></div>
+        <h2 className="outline-heading outline-heading--2">
+          Czas się wyróżnić.
+        </h2>
+        <div className="text-container text-container--2">
+          <span>
+            Aby ludzie chcieli korzystać z twoich usług, twój produkt musi być
+            naj. Najlepszy, najdokładniejszy, najszybszy…To unikalna cecha
+            twojego produktu sprawi, że stanie się on pożądany.
+          </span>
+        </div>
+        <div className="line line-6">
+          <p className="scroll">scroll</p>
+        </div>
+      </section>
+      <section className="section">
+        <div className="line line-3"></div>
+        <h2 className="outline-heading outline-heading--2">
+          Czas przestać prosić.
+        </h2>
+        <div className="text-container text-container--2">
+          <span>
+            Nikt nie jest w stanie stworzyć usługi dla wszystkich. Zamiast
+            prosić każdego, by wypróbował twój produkt, postaraj się zadbać o
+            grupę klientów, która będzie aktywnie uczestniczyć w działaniach
+            twojej firmy.
           </span>
         </div>
         <div className="line line-4"></div>
